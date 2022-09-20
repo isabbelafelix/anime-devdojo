b2ecd3aad741984662afe479bf81092d0d3b8419
Isabela Felix <isabbelafelix@gmail.com>
2022-09-20 14:56:31 -0300

    first commit

---
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -0,0 +1,27 @@
+package academy.devdojo.springboot2.controller;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/animes")
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+
+    @GetMapping
+    public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Boku no Hero"), new Anime("Berserk"));
+    }
+}
---
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -0,0 +1,11 @@
+package academy.devdojo.springboot2.service;
+
+import academy.devdojo.springboot2.domain.Anime;
+
+import java.util.List;
+
+public class AnimeService {
+    public List<Anime> listAll() {
+
+    }
+}
